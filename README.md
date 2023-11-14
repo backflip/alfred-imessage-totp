0ca2469862d50188df0ef6cbd555f74ece4690f8
Thomas Jaggi <thomas@responsive.ch>
2023-11-14 17:31:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# alfred-imessage-totp
+
+Extract tokens from iMessage.
+
+This Alfred workflow searches your local iMessage database for messages within the last 5 minutes containing potential TOTPs.
---
+++ index.js
@@ -0,0 +1,39 @@
+import { homedir } from "node:os";
+import { resolve } from "node:path";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+const DB_PATH = resolve(homedir(), "Library/Messages/chat.db");
+const MINUTES = 5;
+
+const db = await open({
+  filename: DB_PATH,
+  driver: sqlite3.Database,
+});
+
+const messages = await db.all(`
+  select * from message 
+  where message.is_from_me = 0
+    and message.text is not null
+    and length(message.text) > 0
+    and datetime(message.date / 1000000000 + strftime('%s', '2001-01-01'), 'unixepoch', 'localtime') >= datetime('now', '-${MINUTES} minutes', 'localtime')
+`);
+
+const codes = messages
+  .map((message) => {
+    // 🤷‍♂️
+    const [code] = message.text.match(/\d{4,}/) || [];
+
+    return code;
+  })
+  .filter(Boolean);
+
+console.log(
+  JSON.stringify({
+    items: codes.length
+      ? codes.map((code) => ({
+          title: code,
+        }))
+      : [{ title: `No results in last ${MINUTES} mins` }],
+  })
+);
